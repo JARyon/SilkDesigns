922717000a5980e9e5bc19d3cd8faec82e7173c8
John A Ryon <118782692+JARyon@users.noreply.github.com>
2022-11-28 22:47:48 -0500

    Add files via upload

---
+++ ADO/Models/InventoryIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ADO.Models;
+namespace ADO.Models
+{
+    public class InventoryIndexViewModel
+    {
+        public Inventory Inventory { get; set; }    
+        public Size Size { get; set; }  
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Code { get; set; }
+    }
+}
